538974ecba8b916a4e65cba6829de506019c32e7
augdiebold <aug.diebold@gmail.com>
2023-10-14 14:10:06 -0300

    first commit

---
+++ minha_escola/minha_escola/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from minha_escola.users.models import User
+from minha_escola.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
